452ef93af78af1124d42446f777b4c9fa9f749ff
Ashish Newar <122523482+AshNewar@users.noreply.github.com>
2023-06-27 19:08:40 +0530

    Update orderFunc.js

--- controllers/orderFunc.js
+++ controllers/orderFunc.js
@@ -120,8 +120,9 @@ export const myOrder = TryCatch(async (req, res, next) => {
 
 export const orderDetails = async (req, res, next) => {
   try {
-    const order = await Order.findById(req.params.id);
     console.log(req.params.id);
+    
+    const order = await Order.findById(req.params.id);
 
     if (!order) return next(new ErrorHandler("Invalid OrderId", 401));
     console.log(order);
